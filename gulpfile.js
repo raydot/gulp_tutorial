0fe0da349aff4dc037439311e159494975598ff8
Dave Kanter <moatgoat@hotmail.com>
2018-06-22 13:29:41 -0700

    Gulp working, added images task.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+// Gulp.js configuration
+
+var 
+    //modules
+    gulp = require('gulp'),
+    newer = require('gulp-newer'),
+    imagemin = require('gulp-imagemin'),
+
+    //development mode?
+    devBuild = (process.env.NODE_ENV !== 'production'),
+
+    //folders
+    folder = {
+        src: 'src/',
+        build: 'build/'
+    }
+;
+
+
+// image processing
+gulp.task('images', function() {
+    var out = folder.build + 'images/';
+    return gulp.src(folder.src + 'images/**/*')
+        .pipe(newer(out))
+        .pipe(imagemin({ optimizationLevel: 5 }))
+        .pipe(gulp.dest(out));
+});
